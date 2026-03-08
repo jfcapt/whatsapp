6e2b330e2b89e10f2324fd6ee6ce6b8d23014224
john capt <john.capt@outlook.com.au>
2026-03-08 18:38:22 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
 
-const isProd = process.env.BUILD_MODE === 'prod'
 export default defineConfig({
   base: '/whatsapp/',   // deploying to github pages, so we need to set the base path
   plugins: [
     react(),
-    sourceIdentifierPlugin({
-      enabled: !isProd,
-      attributePrefix: 'data-matrix',
-      includeProps: true,
-    })
   ],
   resolve: {
     alias: {

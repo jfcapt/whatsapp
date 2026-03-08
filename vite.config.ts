fb30bbc144404fca787a42a196986cc88c41f622
john capt <john.capt@outlook.com.au>
2026-03-08 18:47:03 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/whatsapp/',   // deploying to github pages, so we need to set the base path
+  base: process.env.NODE_ENV === 'production' ? '/whatsapp/' : '/',
   plugins: [
     react(),
   ],
